95648f1418e19dcd6ab3fd4dc4b6215f2dc4f355
Daniel <daniel.streby@gmail.com>
2015-03-06 15:05:17 -0500

    Goddamn tabs...

--- src/array_clock.c
+++ src/array_clock.c
@@ -25,20 +25,20 @@ const uint8_t num[10][5] = {
 };
 
 const uint8_t colon[5] = {
-	0b0,0b1,0b0,0b1,0b0 // :
+  0b0,0b1,0b0,0b1,0b0 // :
 };
 
 #endif
 
 struct BIT_INFO {
-  uint16_t s4	:1; // bit 0
-  uint16_t d4	:3; // bit 1-3
-  uint16_t s3	:1; // bit 4
-  uint16_t d3	:3; // bit 5-7
-  uint16_t s2	:1; // bit 8
-  uint16_t d2	:3; // bit 9-11
-  uint16_t s1	:1; // bit 12
-  uint16_t d1	:3; // bit 13-15
+  uint16_t s4 :1; // bit 0
+  uint16_t d4 :3; // bit 1-3
+  uint16_t s3 :1; // bit 4
+  uint16_t d3 :3; // bit 5-7
+  uint16_t s2 :1; // bit 8
+  uint16_t d2 :3; // bit 9-11
+  uint16_t s1 :1; // bit 12
+  uint16_t d1 :3; // bit 13-15
 };
 
 union HardwareRegister {
@@ -72,66 +72,66 @@ void printBits(size_t const size, void const * const ptr)
 
 void writeRow(int row, union HardwareRegister *col)
 {
-	/*
-	int i;
-	for(i=0; i < sizeof(RowRegister) / sizeof(int); i++)
-	{
-		if(i == row)
-		{
-			RowRegister[i] = 1;
-		} else {
-			RowRegister[i] = 0;
-		}
-	}
-	
-	updateShiftRegister(*col.bytes[0]);
-	updateShiftRegister(*col.bytes[1]);
-	delay(2); // Sleep 2ms for persistence of vision
-	*/
+  /*
+  int i;
+  for(i=0; i < sizeof(RowRegister) / sizeof(int); i++)
+  {
+    if(i == row)
+    {
+      RowRegister[i] = 1;
+    } else {
+      RowRegister[i] = 0;
+    }
+  }
+  
+  updateShiftRegister(*col.bytes[0]);
+  updateShiftRegister(*col.bytes[1]);
+  delay(2); // Sleep 2ms for persistence of vision
+  */
 
-	printBits(sizeof(*col), col);
+  printBits(sizeof(*col), col);
 }
 
 void buildRows(int hour, int min)
 {
-	// Separate the hour and minute digits
-	uint8_t h1 = (hour / 10) % 10;
-	uint8_t h2 = hour % 10;
-	uint8_t m1 = (min / 10) % 10;
-	uint8_t m2 = min % 10;
+  // Separate the hour and minute digits
+  uint8_t h1 = (hour / 10) % 10;
+  uint8_t h2 = hour % 10;
+  uint8_t m1 = (min / 10) % 10;
+  uint8_t m2 = min % 10;
 
   union HardwareRegister reg;
 
-	int i;
-	for(i=0; i < 5; i++)
-	{
-		reg.word = 0;
-		reg.bits.d1 = num[h1][i];
-		reg.bits.d2 = num[h2][i];
-		reg.bits.d3 = num[m1][i];
-		reg.bits.d4 = num[m2][i];
-		//reg.bits.s2 = colon[i];
+  int i;
+  for(i=0; i < 5; i++)
+  {
+    reg.word = 0;
+    reg.bits.d1 = num[h1][i];
+    reg.bits.d2 = num[h2][i];
+    reg.bits.d3 = num[m1][i];
+    reg.bits.d4 = num[m2][i];
+    //reg.bits.s2 = colon[i];
 
-		writeRow(i, &reg);
-	}
+    writeRow(i, &reg);
+  }
 }
 
 int main(int argc, const char* argv[])
 {
-	time_t now;
-	struct tm *now_tm;
-	int hour, min;
+  time_t now;
+  struct tm *now_tm;
+  int hour, min;
 
-	while(1)
-	{
-		now = time(NULL);
-		now_tm = localtime(&now);
-		hour = now_tm->tm_hour;
-		min = now_tm->tm_min;
+  while(1)
+  {
+    now = time(NULL);
+    now_tm = localtime(&now);
+    hour = now_tm->tm_hour;
+    min = now_tm->tm_min;
 
-		buildRows(hour, min);
-		sleep(1);
-	}
+    buildRows(hour, min);
+    sleep(1);
+  }
   
   return 0;
 }
